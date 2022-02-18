31cf077c81646c67bdbf448861dd8373feea704c
Steven De Ryck <steven@inform.be>
2022-02-19 00:02:52 +0100

    OOP Deel 2 - Einde Chapter 9

--- bootstrap.php
+++ bootstrap.php
@@ -8,6 +8,12 @@ require __DIR__.'/lib/BattleResult.php';
 require __DIR__.'/lib/BattleManager.php';
 require __DIR__.'/lib/ShipLoader.php';
 
+$configuration = array(
+    'db_dsn'  => 'mysql:host=localhost;dbname=oo_battle',
+    'db_user' => 'root',
+    'db_pass' => null,
+);
+
 
 
 
--- lib/ShipLoader.php
+++ lib/ShipLoader.php
@@ -4,13 +4,24 @@ class ShipLoader
 {
     private $pdo;
 
+    private $dbDsn;
+    private $dbUser;
+    private $dbPass;
+
+    public function __construct($dbDsn, $dbUser, $dbPass)
+    {
+        $this->dbDsn = $dbDsn;
+        $this->dbUser = $dbUser;
+        $this->dbPass = $dbPass;
+    }
+
     /**
      * @return PDO
      */
     private function getPDO()
     {
         if ($this->pdo === null) {
-            $this->pdo = new PDO('mysql:host=localhost;dbname=oo_battle', 'root');
+            $this->pdo = new PDO($this->dbDsn, $this->dbUser, $this->dbPass);
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
         return $this->pdo;
