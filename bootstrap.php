a429663c2986f38793f76a0a7c1077be09e5e323
Steven De Ryck <steven@inform.be>
2022-02-19 00:34:35 +0100

    OOP Deel 2 - Einde Chapter 13

--- bootstrap.php
+++ bootstrap.php
@@ -1,13 +1,13 @@
 <?php
 
 // models
-require_once __DIR__.'/lib/Ship.php';
-require_once __DIR__.'/lib/BattleResult.php';
+require_once __DIR__.'/lib/Model/Ship.php';
+require_once __DIR__.'/lib/Model/BattleResult.php';
 
 // services
-require_once __DIR__.'/lib/BattleManager.php';
-require_once __DIR__.'/lib/ShipLoader.php';
-require_once __DIR__.'/lib/Container.php';
+require_once __DIR__.'/lib/Service/BattleManager.php';
+require_once __DIR__.'/lib/Service/ShipLoader.php';
+require_once __DIR__.'/lib/Service/Container.php';
 
 $configuration = array(
     'db_dsn'  => 'mysql:host=localhost;dbname=oo_battle',
